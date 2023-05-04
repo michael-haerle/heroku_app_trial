cd41e2d5da1f099e85d3850136714c3092c6a002
Michael Haerle <michael.haerle01@gmail.com>
2023-05-04 16:01:04 -0500

    added more facts

--- app.py
+++ app.py
@@ -16,7 +16,13 @@ my_list = ['Chameleons are a family of lizards.',
            'Chameleons are found primarily in Africa, Madagascar, and parts of Asia and Europe.', 
            'Chameleons have a unique tongue structure that allows them to shoot their tongues out at prey at high speeds.', 
            'Chameleons are cold-blooded reptiles and require a warm environment to regulate their body temperature.', 
-           'Female chameleons are usually larger than males, and they lay eggs to reproduce.']
+           'Female chameleons are usually larger than males, and they lay eggs to reproduce.', 
+           'A chameleon’s tongue is incredibly fast, catching prey within a fraction of a second and exceeding speeds of 41g force, according to ExoticDirect, the lead exotic pet insurer in the UK, making it nearly impossible for their next meal to escape.',
+           'Most lizards have the ability to regenerate a severed tail, but not chameleons. Their tail essentially acts like another limb and is not designed to be able to break off, so if it does they will, unfortunately, never have another.',
+           'Like a spider using its sticky web for prey, chameleons use their spit to coat the prey they catch with their tongues and pull them inwards to their mouth. Their spitis400 times stickier than a human’s.',
+           'The heart of chameleons has three chambers: two atria and one ventricle.',
+           'Chameleons also can see ultraviolet light, which is not visible to humans.',
+           'Chameleons that have been placed under ultraviolet lights may be more likely to reproduce, and are more sociable.']
 
 def get_random_element(lst):
     return random.choice(lst)
